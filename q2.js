6193914753b2306108e2fe634306bf129e74cd2b
ahsinam171 <ahsinam171@gmail.com>
2023-05-14 14:29:40 +0545

    Question Number 2

--- q2.js
+++ q2.js
@@ -5,6 +5,9 @@ Name: ABC School of Schoolery, Date: 2000
 Name: BCD School of Trickery, Date: 2006
  */
 
+const answerOf2 = document.getElementById("answer2");
+const questionNo2 = document.querySelector("#q2");
+
 const myIntro = {
   name: "Manisha KC",
   address: "Kaushaltar",
@@ -17,4 +20,28 @@ const myIntro = {
   ],
 };
 
-myIntro.education.forEach((value) => console.log(value));
+const educationList = () => {
+  let result = myIntro.education;
+  return result;
+};
+
+questionNo2.addEventListener("click", (e) => {
+  const educationResult = educationList();
+  const answer = document.createDocumentFragment();
+  console.log(educationResult);
+
+  const schoolElement = document.createElement("li");
+  schoolElement.textContent = `name: ${educationResult[0].name},  year: ${educationResult[0].year}`;
+
+  const highSchoolElement = document.createElement("li");
+  highSchoolElement.textContent = `name: ${educationResult[1].name},  year: ${educationResult[1].year}`;
+
+  const universityElement = document.createElement("li");
+  universityElement.textContent = `name: ${educationResult[2].name},  year: ${educationResult[2].year}`;
+
+  answer.appendChild(schoolElement);
+  answer.appendChild(highSchoolElement);
+  answer.appendChild(universityElement);
+
+  answerOf2.appendChild(answer);
+});
